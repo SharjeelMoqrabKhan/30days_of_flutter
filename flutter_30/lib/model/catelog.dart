b73dd7bcb089854b2c10542f15a00259735c38ec
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-04-22 15:53:32 +0500

    day10 completed

---
+++ flutter_30/lib/model/catelog.dart
@@ -0,0 +1,21 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final  product =[
+  Item(
+    id: 'Mob-real-12',
+    name: 'RealMe 9C',
+    desc: 'RealMe 9c mobile by Xaiomy',
+    color:'Blue',
+    price: 179000,
+    image: 'https://www.whatmobile.com.pk/admin/images/Xiaomi/XiaomiRedmi9C3GB-b.jpg',
+  )
+];
