3a104690bdb659f051f49695cc8eb8897577cc30
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-04-24 12:38:28 +0500

    day13 completed

--- flutter_30/lib/pages/home_page.dart
+++ flutter_30/lib/pages/home_page.dart
@@ -1,9 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_30/model/catelog.dart';
 import 'package:flutter_30/widgets/ItemWidget.dart';
 import 'package:flutter_30/widgets/my_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    final catelogJson = await rootBundle.loadString('assets/files/catelog.json');
+    final decodedData =jsonDecode(catelogJson);
+    var productData=decodedData["products"];
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => Catelog.items[0]);
